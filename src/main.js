c466f7a9455626a5a9e43cb31a2847dfcb5d11c4
Ricardo Antonio Ampuero Ramírez <ricardoantonioampueroramirez@MacBook-Pro-de-Ricardo-2.local>
2024-05-20 21:32:04 -0400

    clase 03: config entorno de desarrollo

---
+++ src/main.js
@@ -0,0 +1,3 @@
+// console.log('hola soy tu padre')
+
+console.log('API URL = https://luhiu.com/ahiahiahiu?api_key=' + API_KEY)
---
+++ src/secrets.example.js
@@ -0,0 +1,2 @@
+// archivo ejemplo crear archivo único con API KEY
+const API_KEY = ''
